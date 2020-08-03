87a0bd4488ccf6c3dc0cfa4f7335c0016236a24c
Willem Pienaar <git@willem.co>
2020-08-03 22:19:09 +0800

    Add installation

--- infra/scripts/install-helm.sh
+++ infra/scripts/install-helm.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -e
 readonly HELM_URL=https://storage.googleapis.com/kubernetes-helm
-readonly HELM_TARBALL=helm-v2.16.9-linux-amd64.tar.gz
+readonly HELM_TARBALL="helm-${HELM_VERSION}-linux-amd64.tar.gz"
 readonly STABLE_REPO_URL=https://kubernetes-charts.storage.googleapis.com/
 readonly INCUBATOR_REPO_URL=https://kubernetes-charts-incubator.storage.googleapis.com/
-curl --user-agent curl-ci-sync -sSL -o "$HELM_TARBALL" "$HELM_URL/$HELM_TARBALL"
-tar xzfv "$HELM_TARBALL"
-PATH="$(pwd)/linux-amd64/:$PATH"
-helm init --client-only
+curl -s "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | tar -C /tmp -xz
+mv /tmp/linux-amd64/helm /usr/bin/helm
 helm repo add incubator "$INCUBATOR_REPO_URL"
